9e8fbbd69e4d4040046175041f4515884b0b6d40
Radu Matei <matei.radu94@gmail.com>
2016-05-08 13:00:52 +0300

    Added DepositTransaction

---
+++ src/transaction/DepositTransaction.java
@@ -0,0 +1,55 @@
+package transaction;
+
+import account.Account;
+import account.exceptions.InvalidAccountException;
+import transaction.exceptions.InvalidTransactionAmountException;
+import transaction.exceptions.InvalidTransactionDateException;
+import transaction.exceptions.InvalidTransactionIdException;
+
+import java.util.Date;
+
+/**
+ * Created by Radu on 5/8/16.
+ */
+public class DepositTransaction extends Transaction {
+
+    private Account targetAccount;
+
+    public DepositTransaction(String transactionId,
+                              Date transactionDate,
+                              Double transactionAmount,
+                              Account targetAccount) throws InvalidTransactionDateException,
+                                                            InvalidTransactionIdException,
+                                                            InvalidTransactionAmountException,
+                                                            InvalidAccountException {
+        super(transactionId, transactionDate, transactionAmount);
+
+        this.setTargetAccount(targetAccount);
+    }
+
+    public DepositTransaction(){
+
+    }
+
+    public void setTargetAccount(Account targetAccount) throws InvalidAccountException {
+        if (targetAccount == null)
+            throw new InvalidAccountException("The target account cannot be null!");
+
+        this.targetAccount = targetAccount;
+    }
+
+    public Account getTargetAccount(){
+        return targetAccount;
+    }
+
+    @Override
+    public void execute() throws Exception {
+        if(targetAccount == null)
+            throw new InvalidAccountException("The target account cannot be null!");
+
+        if ((transactionAmount == null) || (transactionAmount <= 0))
+            throw new InvalidTransactionAmountException("The deposited transaction cannot be null, empty or negative");
+
+        targetAccount.setBalance(targetAccount.getBalance() + transactionAmount);
+    }
+}
