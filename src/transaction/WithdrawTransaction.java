2190204ed2130a546ec8d5f3fc30e6e623b45b48
Radu Matei <matei.radu94@gmail.com>
2016-05-08 13:43:41 +0300

    Added withdraw transaction

---
+++ src/transaction/WithdrawTransaction.java
@@ -0,0 +1,54 @@
+package transaction;
+
+import account.Account;
+
+import account.exceptions.*;
+import transaction.exceptions.*;
+
+import java.util.Date;
+
+/**
+ * Created by Radu on 5/8/16.
+ */
+public class WithdrawTransaction extends Transaction {
+
+    private Account targetAccount;
+
+    public WithdrawTransaction(String transactionId,
+                              Date transactionDate,
+                              Double transactionAmount,
+                              Account targetAccount) throws InvalidTransactionDateException,
+                                                            InvalidTransactionIdException,
+                                                            InvalidTransactionAmountException,
+                                                            InvalidAccountException {
+        super(transactionId, transactionDate, transactionAmount);
+
+        this.setTargetAccount(targetAccount);
+    }
+
+    public WithdrawTransaction(){
+
+    }
+
+    public void setTargetAccount(Account targetAccount) throws InvalidAccountException {
+        if (targetAccount == null)
+            throw new InvalidAccountException("The target account cannot be null!");
+
+        this.targetAccount = targetAccount;
+    }
+
+    public Account getTargetAccount(){
+        return targetAccount;
+    }
+
+    @Override
+    public void execute() throws Exception {
+        if(targetAccount == null)
+            throw new InvalidAccountException("The target account cannot be null!");
+
+        if ((transactionAmount == null) || (transactionAmount <= 0))
+            throw new InvalidTransactionAmountException("The deposited transaction cannot be null, empty or negative");
+
+        targetAccount.setBalance(targetAccount.getBalance() - transactionAmount);
+    }
+}
