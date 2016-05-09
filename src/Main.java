c77bfca0802f0ab33a94783e16fbbae9903e0c0d
Radu Matei <matei.radu94@gmail.com>
2016-05-09 12:29:12 +0300

    Added main for transactions

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import account.*;
+
+import transaction.*;
+
+import java.util.Date;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception {
+        TransactionManager transactionManager = TransactionManager.getInstance();
+        TransactionRepository transactionRepository = TransactionRepository.getInstance();
+
+        Account targetAccount = new Account("targetAccountId", 4.0);
+
+        DepositTransaction depositTransaction = new DepositTransaction("depositTransaction",
+                                                                        new Date(),
+                                                                        46.0,
+                                                                        targetAccount);
+        transactionRepository.addTransaction(depositTransaction);
+
+        transactionManager.executeTransaction(depositTransaction);
+        transactionManager.commitTransaction(depositTransaction.getTransactionId());
     }
 }
